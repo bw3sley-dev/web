14499770ccfb96e6eff8d6ab418655e934f46318
Wesley Bernardes <bw3sley@gmail.com>
2024-06-08 18:58:11 -0300

    :green_heart: ci: add zod error message

--- src/env.ts
+++ src/env.ts
@@ -5,4 +5,12 @@ const envSchema = z.object({
   VITE_ENABLE_API_DELAY: z.string().transform((value) => value === 'true'),
 })
 
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.log('❌ Invalid environment variables.')
+
+  throw new Error('Invalid environment variables.')
+}
+
 export const env = envSchema.parse(import.meta.env)
